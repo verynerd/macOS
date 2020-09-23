04454795db3fd29ca9845e845618596b28bdd8e9
sarkrui <sark.rui@gmail.com>
2020-09-23 15:58:31 +0200

    add: genlink && 10_15_6_links

---
+++ genlinks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SYSVER="10_15_6"
+
+FILENAME=$1
+NULLPATH=${FILENAME%/*}
+OUTPUTFILE="${NULLPATH}/${SYSVER}_links.txt"
+OUTPUTCDNFILE="${NULLPATH}/${SYSVER}_cdn_links.txt"
+
+REPOROOT="https://github.com/verynerd/macOS/tree/resources/"
+CDNREPO="https://cdn.jsdelivr.net/gh/verynerd/macOS@resources/"
+
+URLPRE="${REPOROOT}${SYSVER}"
+URLPRECDN="${CDNREPO}${SYSVER}"
+
+
+if [[ -f "$OUTPUTFILE" ]] || [[ -f "$OUTPUTFILE" ]]; then
+	rm $OUTPUTFILE
+	rm $OUTPUTCDNFILE
+fi
+
+while read LINE; 
+do
+# reading each line
+  echo "${URLPRE}/${LINE}" >> $OUTPUTFILE
+  echo "${URLPRECDN}/${LINE}" >> $OUTPUTCDNFILE
+  
+done < $FILENAME	
