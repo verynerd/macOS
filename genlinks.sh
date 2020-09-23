0e3715796fa4356b2e0a2d6a43fd7a953abd872b
sarkrui <sark.rui@gmail.com>
2020-09-24 00:28:20 +0200

    add: Big Sur Beta 7

--- genlinks.sh
+++ genlinks.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-SYSVER="10_15_6"
+SYSVER=$1
+ls ./ > /tmp/files.txt
 
-FILENAME=$1
-NULLPATH=${FILENAME%/*}
-OUTPUTFILE="${NULLPATH}/${SYSVER}_links.txt"
-OUTPUTCDNFILE="${NULLPATH}/${SYSVER}_cdn_links.txt"
-
-REPOROOT="https://raw.githubusercontent.com/verynerd/macOS/resources/"
+REPOROOT="https://github.com/verynerd/macOS/tree/resources/"
 CDNREPO="https://cdn.jsdelivr.net/gh/verynerd/macOS@resources/"
 
 URLPRE="${REPOROOT}${SYSVER}"
 URLPRECDN="${CDNREPO}${SYSVER}"
 
+OUTPUTFILE="/Users/"$(whoami)"/Desktop/${SYSVER}_links.txt"
+OUTPUTCDNFILE="/Users/"$(whoami)"/Desktop/${SYSVER}_cdn_links.txt"
 
 if [[ -f "$OUTPUTFILE" ]] || [[ -f "$OUTPUTFILE" ]]; then
 	rm $OUTPUTFILE
 	rm $OUTPUTCDNFILE
 fi
+
 while read LINE; 
 do
 # reading each line
-  echo "${URLPRE}/${LINE}" >> $OUTPUTFILE
-  echo "${URLPRECDN}/${LINE}" >> $OUTPUTCDNFILE  
-done < $FILENAME	
+  echo "${URLPRE}/${LINE}" >> ${OUTPUTFILE}
+  echo "${URLPRECDN}/${LINE}" >> ${OUTPUTCDNFILE}
+  
+done < /tmp/files.txt
